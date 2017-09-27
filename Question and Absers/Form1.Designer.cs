bb392f1a22b3b40b4c1062cb48526b59b44f0210
Matthews <rc0029042@student.riversidecollege.ac.uk>
2017-09-27 09:49:06 +0100

    Add project files.

---
+++ Question and Absers/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Question_and_Absers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBxName = new System.Windows.Forms.TextBox();
+            this.question = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBxName
+            // 
+            this.txtBxName.Location = new System.Drawing.Point(73, 67);
+            this.txtBxName.Name = "txtBxName";
+            this.txtBxName.Size = new System.Drawing.Size(100, 20);
+            this.txtBxName.TabIndex = 0;
+            this.txtBxName.Text = "Add name here";
+            this.txtBxName.TextChanged += new System.EventHandler(this.Hello_TextChanged);
+            // 
+            // question
+            // 
+            this.question.Location = new System.Drawing.Point(87, 137);
+            this.question.Name = "question";
+            this.question.Size = new System.Drawing.Size(75, 23);
+            this.question.TabIndex = 1;
+            this.question.Text = "question";
+            this.question.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.question);
+            this.Controls.Add(this.txtBxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBxName;
+        private System.Windows.Forms.Button question;
+    }
+}
+
---
+++ Question and Absers/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question_and_Absers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Hello_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello " + txtBxName.Text);
+        }
+    }
+}
